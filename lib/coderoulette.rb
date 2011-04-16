f95ab0d0f67e05fa286fd03d17a2066f104fdd1e
Joshua Szmajda <josh@optoro.com>
2011-04-16 11:29:05 -0400

    cheesy sketch

--- lib/coderoulette.rb
+++ lib/coderoulette.rb
@@ -2,7 +2,16 @@
 
 #And a shoes app:
 
-Shoes.app do
+Shoes.app :width => 600, :height => 400 do
   # heeeeeeyyyyy-yaaaa
+  background "#CCC".."#000"
+
+  stack :margin => 0.1 do
+    stroke "#333"
+    fill "#333"
+    title "File:"
+    title "Line:"
+    title "Committer:"
+  end
 end
 
