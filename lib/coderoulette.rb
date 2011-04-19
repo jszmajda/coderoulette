627f54742875af13b7b2d7bf418f0590145817b4
Joshua Szmajda <josh@optoro.com>
2011-04-18 22:36:23 -0400

    made prettier

--- lib/coderoulette.rb
+++ lib/coderoulette.rb
@@ -4,37 +4,41 @@ require File.join(File.dirname(__FILE__), 'coderoulette', 'croupier')
 
 
 #And a shoes app:
-Shoes.app :width => 800, :height => 300 do
+Shoes.app :width => 500, :height => 300 do
   croupier = Croupier.new(self)
   # heeeeeeyyyyy-yaaaa
   background "#CCC".."#88A"
 
-  flow do
-    stack :width => 100 do
-      stroke "#333"
-      fill "#333"
+  stack  do
+    stroke "#333"
+    fill "#333"
+    flow do
       caption "Project:"
-      caption "File:"
-      caption "Line:"
-      caption "Committer:"
-
-      button "Pick project folder" do
-        croupier.set_project_path ask_open_folder
-      end
-
-      button "Spin Wheel!" do
-        croupier.spin_wheel
-      end
-
-      button "Open Editor" do
-        croupier.open_editor ENV['EDITOR']
-      end
+      croupier.project_target   = caption ""
     end
-    stack :width => 700 do
-      croupier.project_target   = caption "", :width => 700, :height => 30
-      croupier.file_target      = caption "", :width => 700, :height => 30
-      croupier.line_target      = caption "", :width => 700, :height => 30
-      croupier.committer_target = caption "", :width => 700, :height => 30
+    flow do
+      caption "File:"
+      croupier.file_target      = caption ""
+    end
+    flow do
+      caption "Line:"
+      croupier.line_target      = caption ""
+    end
+    flow do
+      caption "Committer:"
+      croupier.committer_target = caption ""
+    end
+
+    button "Pick project folder" do
+      croupier.set_project_path ask_open_folder
+    end
+
+    button "Spin Wheel!" do
+      croupier.spin_wheel
+    end
+
+    button "Open Editor" do
+      croupier.open_editor ENV['EDITOR']
     end
   end
 end
